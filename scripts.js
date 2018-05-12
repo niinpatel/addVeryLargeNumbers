ab77cdb8c0c9fe964046db3d0d4f1b6361ee46a5
Nitin Patel <nitinpatel278@gmail.com>
2018-05-13 01:23:51 +0530

    comments and refactoring

--- scripts.js
+++ scripts.js
@@ -5,42 +5,48 @@
  * add("15", "15");  // returns "30"
  *
  */
-function add(s1, s2) {
+function add(str1, str2) {
 
-    let sum = "";
+    let sum = "";  // our result will be stored in a string.
 
-    let s1l = s1.length;
-    let s2l = s2.length;
+    // we'll need these in the program many times.
+    let str1Length = str1.length;
+    let str2Length = str2.length;
 
-    let larger = (s1l > s2l) ? s1 : s2;
-    let smaller = (larger === s1) ? s2 : s1;
-    let largerl = larger.length;
-    let smallerl = smaller.length;
+    // if s2 is longer than s1, swap them.
+    if(str2Length > str1Length ){
+        let temp = str1;
+        str2 = str1;
+        str1 = temp;
+    }
 
-    let carry = 0;
+    let carry = 0;  // number that is carried to next decimal place, initially zero.
     let a;
     let b;
     let temp;
     let digitSum;
-    for (let i = 0; i < largerl; i++) {
-        a = parseInt(larger.charAt(largerl - 1 - i));
-        b = parseInt(smaller.charAt(smallerl - 1 - i));
-        b = (b) ? b : 0;
-        temp = (carry + a + b).toString();
-        digitSum = temp.charAt(temp.length - 1);
-        carry = parseInt(temp.substr(0, temp.length - 1));
-        carry = (carry) ? carry : 0;
+    for (let i = 0; i < str1Length; i++) {
+        a = parseInt(str1.charAt(str1Length - 1 - i));      // get ith digit of str1 from right, we store it in a
+        b = parseInt(str2.charAt(str2Length - 1 - i));      // get ith digit of str2 from right, we store it in b
+        b = (b) ? b : 0;                                    // make sure b is a number, (this is useful in case, str2 is shorter than str1
+        temp = (carry + a + b).toString();                  // add a and b along with carry, store it in a temp string.
+        digitSum = temp.charAt(temp.length - 1);            //
+        carry = parseInt(temp.substr(0, temp.length - 1));  // split the string into carry and digitSum ( least significant digit of abSum.
+        carry = (carry) ? carry : 0;                        // if carry is not number, make it zero.
 
-        sum = (i === largerl - 1) ? temp + sum : digitSum + sum;
+        sum = (i === str1Length - 1) ? temp + sum : digitSum + sum;  // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
 
     }
 
-    return sum;
+    return sum;     // return sum
 
 }
 
+console.log(add("2750", "3750"));  // will return 6500
+
 // demo of how the function works
-console.log(add("5987342879234789234897", "23489072349807239487"));  // will return 6010831951584596474384
+// console.log(add("5987342879234789234897", "23489072349807239487"));  // will return 6010831951584596474384
+//
+//
+// console.log(add("893427328497983427893248932498034289324", "234859234879342897893427893274"));  // will return 893427328732842662772591830391462182598
 
-
-console.log(add("893427328497983427893248932498034289324", "234859234879342897893427893274"));  // will return 893427328732842662772591830391462182598
